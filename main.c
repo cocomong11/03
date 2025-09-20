2a5d552991b2f0535c654e0721669b1e8d176b8a
cocomong11 <carpediem1011@naver.com>
2025-09-20 21:39:06 +0900

    03

--- main.c
+++ main.c
@@ -4,13 +4,16 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char c;
-	int i;
-	
-	printf("input a number:");
+	char c,ch;
+	int i,in;
+
+	printf("enter a character:");
 	scanf("%c",&c);
-	i=c-'0';
-	printf("The input number is %i\n",i);
+	i=c;
+	in=c+1;
+	ch=in;
+	
+	printf("The next character of %c(%i) is %c (%i)\n",c,i,ch,in);
 
 	return 0;
 }
